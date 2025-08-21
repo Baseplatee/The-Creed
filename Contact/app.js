3cdc5db6d7d64f7aa9fcb1d8c814e8b4f502a1f1
Amir Mansour <amirmvs2448@gmail.com>
2025-08-21 14:48:56 +0330

    updated chars count feature

--- Contact/app.js
+++ Contact/app.js
@@ -1,13 +1,26 @@
 const inputElement = document.getElementById("username");
 const remaningCharsElement = document.querySelector("span");
-const maxLength = inputElement.maxLength
+const maxLength = inputElement.maxLength;
 
-function calculateRemaningChars (event) {
-const inputLength = event.target.value.length
-const remaningChars = maxLength - inputLength
+function calculateRemaningChars(event) {
+  const inputLength = event.target.value.length;
+  const remaningChars = maxLength - inputLength;
 
-remaningCharsElement.textContent = remaningChars
+  remaningCharsElement.textContent = remaningChars;
+
+  if (remaningChars === 0) {
+    inputElement.classList.add("error");
+    remaningCharsElement.add("error");
+  } else if (remaningChars <= 10) {
+    inputElement.classList.add("warning");
+    remaningCharsElement.classList.add("warning");
+
+    inputElement.classList.remove("error");
+    remaningCharsElement.classList.remove("error");
+  } else {
+    inputElement.classList.remove("warning");
+    remaningCharsElement.classList.remove("warning");
+  }
 }
 
-
-inputElement.addEventListener("input", calculateRemaningChars)
+inputElement.addEventListener("input", calculateRemaningChars);
